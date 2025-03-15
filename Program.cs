6d38da69e3a222b4754c13b1ad48c4967931f5a3
marimonteiroo <marismr1509@gmail.com>
2025-03-14 22:28:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("I Wanna Be Yours Arctic Monkeys");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine(@"I wanna be your vacuum cleaner
+Breathing in your dust
+I wanna be your Ford Cortina
+I will never rust
+
+If you like your coffee hot
+Let me be your coffee pot
+You call the shots, babe
+I just wanna be yours
+
+Secrets I have held in my heart
+Are harder to hide than I thought
+Maybe I just wanna be yours
+I wanna be yours, I wanna be yours");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"Wanna be yours
+Wanna be yours
+Wanna be yours
+Wanna be yours
+
+Wanna be yours
+Wanna be yours
+Wanna be yours
+Wanna be yours");
